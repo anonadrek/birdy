43527f3f765fa7aca762ef398bf458817f5115ae
anonadrek <albinviktorlindblom@gmail.com>
2026-04-30 17:04:43 +0200

    build(content): add shared/content module skeleton

--- shared/content/build.gradle.kts
+++ shared/content/build.gradle.kts
@@ -1,1 +1,30 @@
-// Will be filled in by Task 5 (shared:content module setup)
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+}
+
+kotlin {
+    androidTarget()
+    jvm()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(project(":shared:domain"))
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+android {
+    namespace = "se.birdy.content"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
